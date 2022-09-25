2bd3aa0d4ee1ee24e176be2dac1793b72fa6a866
mkhlkzntsv <mkhlkzntsv96@gmail.com>
2022-09-25 16:18:28 +0500

    доработки

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/main.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public'),
-	},
-	devtool: 'source-map',
-	devServer: {
-		hot: false
-	},
-	module: {
-		rules: [
-		{
-			test: /\.js$/,
-			exclude: /(node_modules)/,
-			use: ['babel-loader']
-		}
-		]
-		
-	}
-	
-}
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: false
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
